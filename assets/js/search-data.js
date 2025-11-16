0041393985ea277e069ed5b50a82303a94b46c8c
ryan-biostat <156350327+ryan-biostat@users.noreply.github.com>
2025-11-16 03:56:34 +0000

    Deploying to gh-pages from @ ryan-biostat/ryan-biostat.github.io@6ca4b6da172c3f38b45abf45f410c5dd4160072d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,7 +473,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%67%61%6C%6C%61%67%68%65%72@%6D%63%77.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/ryan-biostat", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -483,25 +490,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/ryan-gallagher-29b4921b8", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0009-0008-3852-9715", "_blank");
         },
       },{
       id: 'light-theme',
