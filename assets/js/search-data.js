a8053158959d9d45e4efaf74ff948c25c28474f2
ryan-biostat <156350327+ryan-biostat@users.noreply.github.com>
2025-11-16 14:56:02 +0000

    Deploying to gh-pages from @ ryan-biostat/ryan-biostat.github.io@c49ca88bb851d50a00bb2b1fe0f34e1d4b087850 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of projects in Biostatistics &amp; Bioinformatics.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
